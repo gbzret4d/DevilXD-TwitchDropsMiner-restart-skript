3a9816b1520c2001688ad1a88479c97cd2fc5fd1
gbzret4d <skapytek@inbox.lv>
2025-10-26 03:28:16 +0100

    Update restart_twitchdrops.sh

--- restart_twitchdrops.sh
+++ restart_twitchdrops.sh
@@ -103,7 +103,6 @@ try_start_systemd_user_session() {
     log "dbus-launch nicht vorhanden. Kann user session nicht starten."
     return 1
   fi
-
   if systemd_user_available; then
     log "Nach dbus-launch ist systemd user-bus verfügbar."
     return 0
@@ -112,7 +111,6 @@ try_start_systemd_user_session() {
     return 1
   fi
 }
-
 
 rotate_log() {
   if [ -f "$LOG_FILE" ] && [ "$(stat -c%s "$LOG_FILE")" -gt "$MAX_LOG_SIZE" ]; then
